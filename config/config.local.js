96e5f66af3685e1267818bed91139402cb014bd7
kuangshp <kuangshp@126.com>
2019-06-24 13:52:03 +0800

    新增.gitignore文件

--- config/config.local.js
+++ config/config.local.js
@@ -1,3 +1,16 @@
 'use strict';
 
-module.exports = () => {};
+module.exports = () => {
+  const config = (exports = {});
+  // 配置mysql
+  config.mysql = {
+    client: {
+      host: 'localhost',
+      port: '3306',
+      user: 'root',
+      password: 'jianshuihen128',
+      database: 'egg_cms',
+    },
+  };
+  return config;
+};
